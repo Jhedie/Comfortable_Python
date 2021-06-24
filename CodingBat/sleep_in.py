0641c9ff1c60d51b5679b1e632439eb65796e7e1
Jedidiah <jeddiahawuku12@gmail.com>
2021-06-24 18:57:41 +0100

    Sleep_in problem solved

---
+++ CodingBat/sleep_in.py
@@ -0,0 +1,9 @@
+#Problem Link:  https://codingbat.com/prob/p173401
+
+def sleep_in(weekday, vacation):
+  if not(weekday):
+    return True
+  elif weekday and vacation:
+    return True
+  return False
+
